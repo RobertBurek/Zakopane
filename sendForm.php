79e0ad99058430c4b38ee8f3209fa8f492d88094
Robert Burek <robertburek@wp.pl>
2022-01-09 13:13:20 +0100

    Refaktor sendFrom.php i footer

--- sendForm.php
+++ sendForm.php
@@ -6,20 +6,20 @@ $phone=isset($_POST['phone']) ? $_POST['phone'] : "";
 $message=isset($_POST['message']) ? $_POST['message'] : "";
 if($name && $email && $phone && $message){
  $headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: 8bit";
- $message_body="Formularz kontaktowy wysłany ze strony www.apartamentyjakhotel.pl\n";
+ $message_body="Formularz kontaktowy wysłany ze strony www.apartamentjakhotel.pl\n";
  $message_body.="Imię i nazwisko: $name\n";
  $message_body.="Adres email: $email\n";
  $message_body.="Numer telefonu: $phone\n\n";
  $message_body.=$message;
- if(mail("Apartament.chalbinskiego@gmail.com","Formularz kontaktowy",$message_body,$headers)){
- $json=array("status"=>1,"msg"=>"<p style='color: #060;font-size: 15px;'>Formularz został pomyślnie wysłany.</p>");
+ if(mail("Apartament.chalbinskiego@gmail.com","ApartamentJakHotel - $name",$message_body,$headers)){
+ $json=array("status"=>1,"msg"=>"<p style='color: #060;font-size: 20px;font-weight: 700;'>Formularz został pomyślnie wysłany.</p>");
  }
  else{
- $json=array("status"=>0,"msg"=>"<p style='color: #F00;font-size: 15px;''>Wystąpił problem z wysłaniem formularza.</p>"); 
+ $json=array("status"=>0,"msg"=>"<p style='color: #F00;font-size: 20px;font-weight: 700;''>Wystąpił problem z wysłaniem formularza.</p>"); 
  }
 }
 else{
- $json=array("status"=>0,"msg"=>"<p style='color: #F00;font-size: 15px;'>Proszę wypełnić wszystkie pola przed wysłaniem.</p>"); 
+ $json=array("status"=>0,"msg"=>"<p style='color: #F00;font-size: 20px;font-weight: 700;'>Proszę wypełnić wszystkie pola przed wysłaniem.</p>"); 
 }
 echo json_encode($json);
 exit;
