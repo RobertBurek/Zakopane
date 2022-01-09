ffee28393e74b264e9ac4cca7ad7521887721663
Robert Burek <robertburek@wp.pl>
2022-01-09 13:21:17 +0100

    Drobna zmiana nazewnictwa

--- js/bgSlider.js
+++ js/bgSlider.js
@@ -1,22 +1,22 @@
-var sliderBackground = document.getElementById("cbp-bislideshow")
-var slidesBackground = sliderBackground.getElementsByTagName("li");
+var sliderBackground = document.getElementById("backgroundShow")
+var slidersBackground = sliderBackground.getElementsByTagName("li");
 
 var next = 0;
 
 function hideBackgroundSlides() {
-    for(var i = 0; i < slidesBackground.length; i++) {
-        slidesBackground[i].style.opacity = "0";
+    for(var i = 0; i < slidersBackground.length; i++) {
+        slidersBackground[i].style.opacity = "0";
     }
 }
 
 function switchBackgroundSlide() {
-    slidesBackground[next].style.opacity = "0";
+    slidersBackground[next].style.opacity = "0";
     next += 1;
-    if(next >= slidesBackground.length) next = 0;
-    slidesBackground[next].style.opacity = "1";
+    if(next >= slidersBackground.length) next = 0;
+    slidersBackground[next].style.opacity = "1";
 }
 
 hideBackgroundSlides();
-slidesBackground[next].style.opacity = "1";
+slidersBackground[next].style.opacity = "1";
 
 setInterval(switchBackgroundSlide, 3000);
