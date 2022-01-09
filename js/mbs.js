dc7b72df6526f3b53392abc49d4193a14cae01ce
Robert Burek <robertburek@wp.pl>
2022-01-09 12:23:33 +0100

    drobne poprawki

--- js/mbs.js
+++ js/mbs.js
@@ -1,27 +1,22 @@
 var sliderBackground = document.getElementById("cbp-bislideshow")
-var slidesBackgroun = sliderBackground.getElementsByTagName("li");
+var slidesBackground = sliderBackground.getElementsByTagName("li");
 
-
-console.log(sliderBackground);
-console.log(slidesBackgroun);
-
-var v = 0;
+var next = 0;
 
 function hideBackgroundSlides() {
-    for(var i = 0; i < slidesBackgroun.length; i++) {
-        slidesBackgroun[i].style.opacity = "0";
+    for(var i = 0; i < slidesBackground.length; i++) {
+        slidesBackground[i].style.opacity = "0";
     }
 }
 
 function switchBackgroundSlide() {
-    slidesBackgroun[v].style.opacity = "0";
-    v += 1;
-    if(v >= slidesBackgroun.length) v = 0;
-    slidesBackgroun[v].style.opacity = "1";
+    slidesBackground[next].style.opacity = "0";
+    next += 1;
+    if(next >= slidesBackground.length) next = 0;
+    slidesBackground[next].style.opacity = "1";
 }
 
-// hideBackgroundSlides();
-// console.log(slides[v].style.opacity);
-slidesBackgroun[v].style.opacity = "1";
+hideBackgroundSlides();
+slidesBackground[next].style.opacity = "1";
 
 setInterval(switchBackgroundSlide, 3000);
