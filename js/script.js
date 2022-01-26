902dd379297878cc290f112f3996e92b52cf3ee2
RobertB <robertburek@wp.pl>
2022-01-26 21:30:39 +0100

    AlbumFoto portrait css zmienne

--- js/script.js
+++ js/script.js
@@ -5,22 +5,36 @@ let heightContainerMyFoto = document.querySelector("#myFoto .container").offsetH
 const thumbnailsBefore = document.getElementsByClassName("thumbnailsBefore");
 const myFoto = document.getElementById("myFoto");
 
+let paddingTopSection;
+let paddingBottomSection;
+let widthMySlider;
+let heightMySlider;
+let widthImgThumbnails;
+let heightImgThumbnails;
+let widthImgSlides;
+let heightImgSlides;
+let paddingTopLiSlides;
+let marginTopLiSlides;
+let heightMyFoto
 
-let paddingTopSection = 40;
-let paddingBottomSection = 40;
-root.style.setProperty(`--paddingTopSection`, `${paddingTopSection}px`);
-root.style.setProperty(`--paddingBottomSection`, `${paddingBottomSection}px`);
 
-let widthMySlider = currentWidth;
-let heightMySlider = Math.floor(currentWidth * 10/16);
-let widthImgThumbnails = Math.floor(heightMySlider/5);
-let heightImgThumbnails = Math.floor(heightMySlider/5);
+if (currentHeight <= currentWidth ) 
+{
+paddingTopSection = 40;
+paddingBottomSection = 40;
+myFoto.style.setProperty(`--paddingTopSection`, `${paddingTopSection}px`);
+myFoto.style.setProperty(`--paddingBottomSection`, `${paddingBottomSection}px`);
+
+widthMySlider = currentWidth;
+heightMySlider = Math.floor(currentWidth * 10/16);
+widthImgThumbnails = Math.floor(heightMySlider/5);
+heightImgThumbnails = Math.floor(heightMySlider/5);
 heightMySlider = heightImgThumbnails * 5;
-let widthImgSlides = currentWidth - widthImgThumbnails * 2;
-let heightImgSlides = heightMySlider;
-let paddingTopLiSlides = Math.floor((currentHeight-heightImgSlides)/2);
-let marginTopLiSlides = heightContainerMyFoto - paddingTopLiSlides;
-let heightMyFoto = heightMySlider + heightContainerMyFoto + paddingTopSection + paddingBottomSection +paddingTopLiSlides;
+widthImgSlides = currentWidth - widthImgThumbnails * 2;
+heightImgSlides = heightMySlider;
+paddingTopLiSlides = Math.floor((currentHeight-heightImgSlides)/2);
+marginTopLiSlides = heightContainerMyFoto - paddingTopLiSlides;
+heightMyFoto = heightMySlider + heightContainerMyFoto + paddingTopSection + paddingBottomSection +paddingTopLiSlides;
 // let paddingTopLiSlides = Math.floor((currentHeight-heightImgSlides)/2);
 root.style.setProperty(`--heightMySlider`, `${heightMySlider}px`);
 root.style.setProperty(`--widthMySlider`, `${widthMySlider}px`);
@@ -33,6 +47,36 @@ myFoto.style.setProperty(`--heightMyFoto`, `${heightMyFoto}px`);
 myFoto.style.setProperty(`--paddingTopLiSlides`, `${paddingTopLiSlides}px`);
 myFoto.style.setProperty(`--marginTopLiSlides`, `${marginTopLiSlides}px`);
 // console.log("pierwsze miejsce");
+} else {
+// paddingTopSection = 0;
+// paddingBottomSection = 0;
+// root.style.setProperty(`--paddingTopSection`, `${paddingTopSection}px`);
+// root.style.setProperty(`--paddingBottomSection`, `${paddingBottomSection}px`);
+
+// widthMySlider = currentWidth;
+// heightMySlider = Math.floor(currentWidth * 10/16);
+// widthImgThumbnails = Math.floor(heightMySlider/5);
+// heightImgThumbnails = Math.floor(heightMySlider/5);
+// heightMySlider = heightImgThumbnails * 5;
+// widthImgSlides = currentWidth - widthImgThumbnails * 2;
+// heightImgSlides = heightMySlider;
+// paddingTopLiSlides = Math.floor((currentHeight-heightImgSlides)/2);
+// marginTopLiSlides = heightContainerMyFoto - paddingTopLiSlides;
+// heightMyFoto = heightMySlider + heightContainerMyFoto + paddingTopSection + paddingBottomSection +paddingTopLiSlides;
+// root.style.setProperty(`--heightMySlider`, `${heightMySlider}px`);
+// root.style.setProperty(`--widthMySlider`, `${widthMySlider}px`);
+// root.style.setProperty(`--widthImgThumbnails`, `${widthImgThumbnails}px`);
+// root.style.setProperty(`--heightImgThumbnails`, `${heightImgThumbnails}px`);
+// root.style.setProperty(`--widthImgSlides`, `${widthImgSlides}px`);
+// root.style.setProperty(`--heightImgSlides`, `${heightImgSlides}px`);
+// myFoto.style.setProperty(`--heightMyFoto`, `${heightMyFoto}px`);
+// myFoto.style.setProperty(`--paddingTopLiSlides`, `${paddingTopLiSlides}px`);
+// myFoto.style.setProperty(`--marginTopLiSlides`, `${marginTopLiSlides}px`);
+
+}
+
+
+
 function altitudeChanged() {
   // console.log("drugie miejsce");
   if ((document.body.offsetHeight != currentHeight) || (document.body.offsetWidth != currentWidth)) {
@@ -42,28 +86,63 @@ function altitudeChanged() {
     // console.log(currentWidth + " - " + currentHeight);
     // console.log(getComputedStyle(root).getPropertyValue("--heightMySlider"));
     // location.reload(true);
-    if ((currentWidth >= 100)&&(currentWidth <= 1000)){
-      var widthMySlider = currentWidth;
-      var heightMySlider = Math.floor(currentWidth * 10/16);
-      var widthImgThumbnails = Math.floor(heightMySlider/5);
-      var heightImgThumbnails = Math.floor(heightMySlider/5);
+
+
+    // if ((currentWidth >= 100)&&(currentWidth <= 1000))
+    if (currentHeight <= currentWidth )
+    {
+      console.log(currentWidth + " - " + currentHeight);
+      paddingTopSection = 40;
+      paddingBottomSection = 40;
+      myFoto.style.setProperty(`--paddingTopSection`, `${paddingTopSection}px`);
+      myFoto.style.setProperty(`--paddingBottomSection`, `${paddingBottomSection}px`);
+      widthMySlider = currentWidth;
+      heightMySlider = Math.floor(currentWidth * 10/16);
+      widthImgThumbnails = Math.floor(heightMySlider/5);
+      heightImgThumbnails = Math.floor(heightMySlider/5);
       heightMySlider = heightImgThumbnails * 5;
-      var widthImgSlides = currentWidth - widthImgThumbnails * 2;
-      var heightImgSlides = heightMySlider;
-      let paddingTopLiSlides = Math.floor((currentHeight-heightImgSlides)/2);
+      widthImgSlides = currentWidth - widthImgThumbnails * 2;
+      heightImgSlides = heightMySlider;
+      paddingTopLiSlides = Math.floor((currentHeight-heightImgSlides)/2);
       heightContainerMyFoto = document.querySelector("#myFoto .container").offsetHeight;
-      let marginTopLiSlides = heightContainerMyFoto - paddingTopLiSlides;
-      var heightMyFoto = heightMySlider + heightContainerMyFoto + 150;
-      root.style.setProperty(`--heightMySlider`, heightMySlider + "px");
-      root.style.setProperty(`--widthMySlider`, widthMySlider + "px");
+      marginTopLiSlides = heightContainerMyFoto - paddingTopLiSlides;
+      heightMyFoto = heightMySlider + heightContainerMyFoto + paddingTopSection + paddingBottomSection +paddingTopLiSlides;
+      root.style.setProperty(`--heightMySlider`, `${heightMySlider}px`);
+      root.style.setProperty(`--widthMySlider`, `${widthMySlider}px`);
       // console.log(thumbnailsBefore);
-      root.style.setProperty(`--widthImgThumbnails`, widthImgThumbnails + "px");
-      root.style.setProperty(`--heightImgThumbnails`, heightImgThumbnails + "px");
-      root.style.setProperty(`--widthImgSlides`, widthImgSlides + "px");
-      root.style.setProperty(`--heightImgSlides`, heightImgSlides + "px");
-      myFoto.style.setProperty(`--heightMyFoto`, heightMyFoto + "px");
-      myFoto.style.setProperty(`--paddingTopLiSlides`, paddingTopLiSlides + "px");
-      myFoto.style.setProperty(`--marginTopLiSlides`, marginTopLiSlides + "px");
+      root.style.setProperty(`--widthImgThumbnails`, `${widthImgThumbnails}px`);
+      root.style.setProperty(`--heightImgThumbnails`, `${heightImgThumbnails}px`);
+      root.style.setProperty(`--widthImgSlides`, `${widthImgSlides}px`);
+      root.style.setProperty(`--heightImgSlides`, `${heightImgSlides}px`);
+      myFoto.style.setProperty(`--heightMyFoto`, `${heightMyFoto}px`);
+      myFoto.style.setProperty(`--paddingTopLiSlides`, `${paddingTopLiSlides}px`);
+      myFoto.style.setProperty(`--marginTopLiSlides`, `${marginTopLiSlides}px`);
+    } else {
+      paddingTopSection = 0; //
+      paddingBottomSection = 0; //
+      myFoto.style.setProperty(`--paddingTopSection`, `${paddingTopSection}px`); //
+      myFoto.style.setProperty(`--paddingBottomSection`, `${paddingBottomSection}px`); //
+      widthMySlider = currentWidth; //
+      heightMySlider = Math.floor(currentWidth * 10/16); //
+      widthImgThumbnails = Math.floor(widthMySlider/5); //
+      heightImgThumbnails = Math.floor(widthMySlider/5); //
+      widthMySlider = widthImgThumbnails * 5; //
+      widthImgSlides = currentWidth; //
+      heightImgSlides = heightMySlider; //
+      paddingTopLiSlides = Math.floor((currentHeight-heightImgSlides)/2);
+      heightContainerMyFoto = document.querySelector("#myFoto .container").offsetHeight;
+      marginTopLiSlides = heightContainerMyFoto - paddingTopLiSlides;
+      heightMyFoto = heightMySlider + heightContainerMyFoto + paddingTopSection + paddingBottomSection +paddingTopLiSlides;
+      root.style.setProperty(`--heightMySlider`, `${heightMySlider}px`);
+      root.style.setProperty(`--widthMySlider`, `${widthMySlider}px`);
+      // console.log(thumbnailsBefore);
+      root.style.setProperty(`--widthImgThumbnails`, `${widthImgThumbnails}px`);
+      root.style.setProperty(`--heightImgThumbnails`, `${heightImgThumbnails}px`);
+      root.style.setProperty(`--widthImgSlides`, `${widthImgSlides}px`);
+      root.style.setProperty(`--heightImgSlides`, `${heightImgSlides}px`);
+      myFoto.style.setProperty(`--heightMyFoto`, `${heightMyFoto}px`);
+      myFoto.style.setProperty(`--paddingTopLiSlides`, `${paddingTopLiSlides}px`);
+      myFoto.style.setProperty(`--marginTopLiSlides`, `${marginTopLiSlides}px`);
     }
   }
   setTimeout("altitudeChanged()",1000);
